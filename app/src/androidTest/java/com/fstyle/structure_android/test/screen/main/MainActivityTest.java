c7b5186ae4f83c996d5e18a874a5e8e5545425a9
daolq3012 <dao.le.2511@gmail.com>
2017-05-12 22:43:14 +0700

    implement BDD

--- app/src/androidTest/java/com/fstyle/structure_android/test/screen/main/MainActivityTest.java
+++ app/src/androidTest/java/com/fstyle/structure_android/test/screen/main/MainActivityTest.java
@@ -3,32 +3,31 @@ package com.fstyle.structure_android.test.screen.main;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import com.fstyle.structure_android.R;
 import com.fstyle.structure_android.screen.main.MainActivity;
 import cucumber.api.CucumberOptions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import junit.framework.Assert;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
 
 /**
  * Created by Sun on 5/7/2017.
  */
-@CucumberOptions(features = "features")
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+@CucumberOptions(features = "features/result.feature")
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
 
-    //    @Rule
-    //    public ActivityTestRule<MainActivity> mTestRule = new ActivityTestRule<>(MainActivity
-    // .class);
-
-    public MainActivityTest(MainActivity activityClass) {
-        super(MainActivity.class);
-    }
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
 
     @Given("^I have a MainActivity")
     public void I_have_a_MainActivity() {
-        Assert.assertNotNull(getActivity());
+        mActivityRule.launchActivity(null);
     }
 
     @When("^I input keyword (\\S+)$")
@@ -46,7 +45,8 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     @Then("^I should see error on the (\\S+)$")
     public void i_should_see_error_on_the_keyword(final String view) {
         // Write code here that turns the phrase above into concrete actions
-        Espresso.onView(ViewMatchers.withId(R.id.edtKeyword)).perform(ViewActions.typeText(view));
+        //        Espresso.onView(ViewMatchers.withId(R.id.edtKeyword)).perform(ViewActions
+        // .typeText(view));
     }
 
     @Then("^I should (true|false) Result Screen")
